2b02c2940a3aa953f760d39b006c700afd6b138f
devaughan <75347649+devaughan@users.noreply.github.com>
2021-12-17 23:31:54 -0500

    finished

--- Day8/SevenSegmentSearchPart2.java
+++ Day8/SevenSegmentSearchPart2.java
@@ -6,12 +6,12 @@ public class SevenSegmentSearchPart2 {
     public static void main(String[] args) {
 
         char[] baseDigit = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
-        
         int sum = 0;
 
         File puzzleInput = new File("C:\\Users\\devau\\OneDrive\\Documents\\Coding\\AdventOfCode\\2021\\Day8\\SevenSegmentSearchPuzzleInput.txt");
         
         try {
+
             Scanner input = new Scanner(puzzleInput);
 
             while(input.hasNextLine()) {
@@ -25,70 +25,143 @@ public class SevenSegmentSearchPart2 {
                 String six = input.next();
                 String seven = input.next();
                 String eight = input.next();
+                String nine = input.next();
 
-                String[] uniqueSignal = {zero, one, two, three, four, five, six, seven, eight};
+                String[] uniqueSignal = {zero, one, two, three, four, five, six, seven, eight, nine};
+                String[] signalSorted = new String[10];
                 
+                // find easy numbers
                 for (String i : uniqueSignal) {
                     // i = 1
                     if (i.length() == 2) {
-                        uniqueSignal[1] = i;
+                        signalSorted[1] = i;
                     }
                     // i = 7
                     else if (i.length() == 3) {
-                        uniqueSignal[7] = i;
+                        signalSorted[7] = i;
                     }
                     // i = 4
                     else if (i.length() == 4) {
-                        uniqueSignal[4] = i;
+                        signalSorted[4] = i;
                     }
                     // i = 8
                     else if (i.length() == 7) {
-                        uniqueSignal[8] = i;
+                        signalSorted[8] = i;
                     }
                 }
 
-
-
-                if (input.next().equals("|")) {
-
-                    String digit1 = input.next();
-                    String digit2 = input.next();
-                    String digit3 = input.next();
-                    String digit4 = input.next();
-                    String[] screenOutput = {digit1, digit2, digit3, digit4};
-
-                    for (int outputIndex = 0; outputIndex < 4; outputIndex++) {
-                        for(int index = 0; index < digits.length; index++) {
-                            if (screenOutput[outputIndex].length() == digits[index]) {
-                                sum++;
-                                break;
-                            }
+                for (String i : uniqueSignal) {
+                    if (i.length() == 6) {
+                        // i = 6
+                        if (!containsAllLetters(i, signalSorted[1])) {
+                            signalSorted[6] = i;
+                        }
+                        // i = 9
+                        else if (containsAllLetters(i, signalSorted[4])) {
+                            signalSorted[9] = i;
+                        }
+                        // i = 0
+                        else {
+                            signalSorted[0] = i;
                         }
                     }
-                    
+                    else if (i.length() == 5) {
+                        // i = 3
+                        if (containsAllLetters(i, signalSorted[1])) {
+                            signalSorted[3] = i;
+                        }
+                        // i = 5
+                        else if (numberOfSharedSides(i, signalSorted[4]) == 3) {
+                            signalSorted[5] = i;
+                        }
+                        // i = 2
+                        else {
+                            signalSorted[2] = i;
+                        }
+                    }
                 }
+
+                // | deliminator
+                input.next();
+                
+
+                String digit1 = input.next();
+                String digit2 = input.next();
+                String digit3 = input.next();
+                String digit4 = input.next();
+
+                String[] screenOutput = {digit1, digit2, digit3, digit4};
+
+                for (int index = 0; index < screenOutput.length; index++) {
+                    for (int count = 0; count < signalSorted.length; count++) {
+                        if (exactCopy(screenOutput[index], signalSorted[count])) {
+                            sum += Math.pow(10, 3 - index) * count;
+                        }
+                    }
+                }
+                
             }
 
             input.close();
 
         }
+
         catch (FileNotFoundException e) {
+
             System.out.println("File does not exist");
+
         }
         
         System.out.println(sum);
 
     }
 
-    public static String compare(String smallString, String largeString) {
-        String s = "";
-        for (int index = 0; index < largeString.length(); index++) {
-            for (int i = 0; i < smallString.length(); i++) {
-                if (largeString.charAt(index) != smallString.charAt(i)) {
+    public static boolean containsAllLetters(String mainString, String smallString) {
+        boolean hasLetters;
+        for (int smallIndex = 0; smallIndex < smallString.length(); smallIndex++) {
+            hasLetters = false;
+            for (int mainIndex = 0; mainIndex < mainString.length(); mainIndex++) {
+                if (smallString.charAt(smallIndex) == mainString.charAt(mainIndex)) {
+                    hasLetters = true;
+                    break;
                 }
             }
-        }   
-        return 
+            if (!hasLetters) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    public static boolean exactCopy(String string1, String string2) {
+        if (string1.length() != string2.length()) {
+            return false;
+        }
+        boolean hasLetters;
+        for (int smallIndex = 0; smallIndex < string2.length(); smallIndex++) {
+            hasLetters = false;
+            for (int mainIndex = 0; mainIndex < string1.length(); mainIndex++) {
+                if (string2.charAt(smallIndex) == string1.charAt(mainIndex)) {
+                    hasLetters = true;
+                    break;
+                }
+            }
+            if (!hasLetters) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int numberOfSharedSides(String string1, String string2) {
+        int sum = 0;
+        for (int i = 0; i < string1.length(); i++) {
+            for (int j = 0; j < string2.length(); j++) {
+                if (string1.charAt(i) == string2.charAt(j)) {
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
 }
